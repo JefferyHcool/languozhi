ac29733529198c277739e63b882f5e766c37ffd6
Jefferyhcool <1063474837@qq.com>
2024-12-30 11:09:09 +0800

    chore: 项目初始化

--- languozhi-frontend/.eslintrc.js
+++ languozhi-frontend/.eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    node: true
   },
   extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
